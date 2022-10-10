c8ef3c5870ee6af3661238c28bb78407a4900ca0
swathiravi-vh <swathiit01@gmail.com>
2022-10-10 20:18:08 +0530

    first commit

---
+++ src/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div>
+        <img src='https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2014%2F02%2F27%2F16%2F10%2Fflowers-276014__340.jpg&imgrefurl=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnature%2F&tbnid=gSztbPoa5QhyUM&vet=12ahUKEwifmsu88tX6AhWdKbcAHexiAUUQMygCegUIARDkAQ..i&docid=Ba_eiczVaD9-zM&w=534&h=340&q=nature%20images&ved=2ahUKEwifmsu88tX6AhWdKbcAHexiAUUQMygCegUIARDkAQ'/>
+    </div>
+  )
+}
+
+export default User
